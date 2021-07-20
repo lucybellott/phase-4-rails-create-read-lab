6419a2bcf4e2ac8e35c8001f3ebe58a3cf112ce3
Lucy <lucybellott@gmail.com>
2021-07-20 19:55:14 -0400

    commit tree f11bd546f5b59b55bc9be2914231a1c03e34072e

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,16 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    def show
+        plants = Plant.find(params[:id])
+        render json: plants
+    end
+
+    def create
+        plants = Plant.create(name: params[:name], image: params[:image], price: params[:price])
+        render json: plants, status: :created
+    end
+end
